45475499e1115e33c89ada8679d47a3c5a822355
tl2488 <tyler.law77@gmail.com>
2017-07-08 22:18:49 -0400

    Deleting project files and added import data code

--- main.R
+++ main.R
@@ -1,1 +1,6 @@
+# Including packages ---------------------------
+library(gdata)
+
 # Load data ---------------------------
+diabetes_df <- read.xls("Data.xlsx", sheet = "Diabetes")
+kyphosis_df <- read.xls("Data.xlsx", sheet = "Kyphosis")
